0afa5c2838e0f46a62268fd167825e41c8c0fe98
tsellers1990 <tsellers1990@gmail.com>
2020-11-04 18:36:17 -0700

    bbbb

--- logic.js
+++ logic.js
@@ -1,1 +1,14 @@
 //placeholder
+
+button = document.getElementById("#button");
+
+
+function whateverFunction () {
+    console.log("aaaaa")
+}
+
+
+
+
+
+button.addEventListener("click", whateverFunction)
